74f84698230ca30a47ff758762ce489c5fbc694f
ziler <410992025@qq.com>
2018-01-25 19:23:29 +0800

    commits

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>My Todo App</h1>
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+  import TodoList from './components/TodoList'
+
+  export default {
+    components: {
+      TodoList
+    }
+  }
+</script>
+
+<style>
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  #app {
+    max-width: 400px;
+    margin: 0 auto;
+    line-height: 1.4;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #32485F;
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
---
+++ src/components/BaseInputText.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="text" placeholder="New todo" class="input" :value="value" v-on="listeners">
+</template>
+
+<script>
+    export default {
+      props: {
+        value: {
+          type: String,
+          default: ''
+        }
+      },
+      computed: {
+        listeners () {
+          // debugger
+          return {
+            ...this.$listeners,
+            input: event => this.$emit('input', event.target.value)
+          }
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .input {
+    width: 100%;
+    padding: 8px 10px;
+    border: 1px solid #32485F;
+  }
+</style>
